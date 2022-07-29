8ad6a2e7c7c3ea7ae51827989d6a13a21e5dd177
dano <dano.carroll@constrainttec.com>
2022-07-29 13:09:38 +1000

    Trying base64 encoding of the requests and add certificate support.

--- pointhq/__init__.py
+++ pointhq/__init__.py
@@ -1,1 +1,2 @@
 from .api import Point
+from .exceptions import *
--- pointhq/helpers.py
+++ pointhq/helpers.py
@@ -5,6 +5,7 @@ except ImportError:
 
 import base64
 import httplib2
+import certifi
 
 from . import exceptions
 
@@ -19,22 +20,26 @@ def request(method, url, auth, data=None):
     if data is not None:
         data = json.dumps(data)
 
-    uri_f = "https://pointhq.com" + url
-    auth_s = base64.b64encode(":".join(auth))
+    certificate_location = certifi.where()
+    uri_f = "https://api.pointhq.com" + url
+    auth_s = base64.urlsafe_b64encode((":".join(auth)).encode()).decode()
     headers = {"Accept":"application/json", "Content-Type":"application/json",
             "Authorization":"Basic " + auth_s}
 
-    response, content = httplib2.Http(timeout=10, 
-        ca_certs="/etc/ssl/certs").request(uri=uri_f, method=method.upper(), 
-        body=data, headers=headers)
+    response, content = httplib2.Http(
+        timeout=10, 
+        ca_certs=certificate_location).request(
+            uri=uri_f,
+            method=method.upper(),
+            body=data, headers=headers)
     
     if response.status == 403:
-        raise AccessDeniedError("Access forbidden")
+        raise exceptions.AccessDeniedError("Access forbidden")
 
     if response.status == 404:
-        raise NotFoundError("Resource not found: %s" % uri_f)
+        raise exceptions.NotFoundError("Resource not found: %s" % uri_f)
 
     if response.status == 500:
-        raise PointAPIError(content)
+        raise exceptions.PointAPIError(content)
 
     return Response(response.status, content)
